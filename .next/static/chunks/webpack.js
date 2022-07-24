fab3a6a358f97d18e84bf69111121c24bd6d494d
YuryRegis <yury_regis@hotmail.com>
2022-07-24 17:51:12 -0300

    build: add Roboto custom font

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2deae658b7c4069"; }
+/******/ 		__webpack_require__.h = function() { return "970a0e23be08e64e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
